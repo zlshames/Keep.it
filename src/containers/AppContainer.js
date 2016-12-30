5761faee807b2d43b52d5dd89e48e0774e6adb70
WhoAteDaCake <doitsimplefy@gmail.com>
2016-12-30 20:51:07 +0000

    Fixed redux errors

--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -1,13 +1,10 @@
 import React, { Component, PropTypes } from 'react'
 import { browserHistory, Router } from 'react-router'
 import { Provider } from 'react-redux'
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 
 class AppContainer extends Component {
   static propTypes = {
     routes : PropTypes.object.isRequired,
-    store  : PropTypes.object.isRequired
   }
 
   shouldComponentUpdate () {
@@ -18,13 +15,9 @@ class AppContainer extends Component {
     const { routes, store } = this.props
 
     return (
-      <Provider store={store}>
-        <MuiThemeProvider muiTheme={getMuiTheme()}>
-          <div style={{ height: '100%' }}>
-            <Router history={browserHistory} children={routes} />
-          </div>
-        </MuiThemeProvider>
-      </Provider>
+      <div style={{ height: '100%' }}>
+        <Router history={browserHistory} children={routes} />
+      </div>
     )
   }
 }
