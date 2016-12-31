9486cf25dc9019647bd5f9f701b72c287cbc2014
WhoAteDaCake <doitsimplefy@gmail.com>
2016-12-31 15:37:28 +0000

    Moved Note to a seperate component Added new methods for notes

--- src/components/Note/Note.js
+++ src/components/Note/Note.js
@@ -1,42 +1,18 @@
 import React from 'react'
-import superagent from "superagent"
+import { Link } from 'react-router'
 
-import './App.scss'
-import Note from "../../../components/Note";
+import "./Note.scss";
 
-// TODO:
-//ADD undo method for deleted notes
-//Extract the Note to a different component
-//	Add shouldcomponentUpdate for text,id changes
-
-class App extends React.Component {
+class Note extends React.Component {
   constructor() {
 
     super()
     this.saveNote = this.saveNote.bind(this);
 		this.editNote = this.editNote.bind(this);
-		this.grabNotes = this.grabNotes.bind(this);
 		this.state = {
       notes: []
     }
-		this.grabNotes();
-
   }
-	grabNotes() {
-
-		superagent
-      .get('http://localhost:3000/notes')
-      .end((err, res) => {
-
-				if (err) {
-					console.log(err)
-        } else {
-				  this.setState({
-            notes: res.body.data
-          });
-        }
-    })
-	}
   saveNote(e) {
 
     let textBox = e.target.previousSibling,
@@ -88,34 +64,24 @@ class App extends React.Component {
 				if (err) {
 					console.log(err)
 	      } else if(res.body.success) {
-					note.parentElement.removeChild(note);
+					//note.parentElement.removeChild(note);
 				}
 	  })
 	}
 	render() {
 
-    const notes = this.state.notes.map((note, key) =>
-			<Note
-				key = {note._id}
-				id = {note._id}
-				content = {note.content} />
-    );
-
     return (
-      <div className = "app">
-
-			<div className="note">
-				<textarea id="note1"></textarea>
-				<button onClick={this.saveNote} className="btn btn-default">Save me</button>
+			<div className = "note" id = {this.props.id}>
+				<div className = "note__top">
+					<i onClick = {this.deleteNote}  className = "fa fa-times" aria-hidden="true"></i>
+				</div>
+				<textarea
+					onKeyDown = {this.editNote}
+					defaultValue = {this.props.content}>
+				</textarea>
 			</div>
-			
-			<div className = "notes">
-				{notes}
-			</div>
-
-      </div>
     );
   }
 }
 
-export default App
+export default Note
