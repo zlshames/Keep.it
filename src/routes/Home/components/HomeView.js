f21104d4835e3fbc40632b4dff91a5d21acc7fd9
Zach Shames <zlshames@gmail.com>
2017-01-09 12:33:20 -0500

    added welcome home page stuff

--- src/routes/Home/components/HomeView.js
+++ src/routes/Home/components/HomeView.js
@@ -2,8 +2,9 @@ import React from 'react'
 import './HomeView.scss'
 
 export const HomeView = () => (
-  <div>
-		Home
+  <div className="box">
+    <h1 className="welcome">Welcome to Keep.it</h1>
+    <p className="creators">by zlshames & rice</p>
   </div>
 )
 
